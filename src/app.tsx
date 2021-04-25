e4b88458c8e18b650ddc6765f49e15c45186dc0d
Josh Sprow <jsprow@gmail.com>
2021-04-25 18:16:48 -0400

    first commit.

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+  </>,
+  document.getElementById("main")
+);
