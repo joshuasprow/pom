59e0ea09ea5902d4e91de6fecf464b2fd1d8865a
Josh Sprow <jsprow@gmail.com>
2021-04-26 09:36:54 -0400

    adding comments.

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,9 +1,19 @@
 export {};
+
+/**
+ * Extending the global Window interface to enable type safety for Electron
+ * methods.
+ *
+ * https://stackoverflow.com/a/47130953/8340430
+ *
+ **/
+
 declare global {
   interface Window extends Window {
     electron: {
       sendProgress(percent: null | number): void;
     };
   }
+
+  const window: Window;
 }
-declare const window: Window;
--- src/renderer/preload.ts
+++ src/renderer/preload.ts
@@ -1,5 +1,6 @@
 import { contextBridge, ipcRenderer } from "electron";
 
+// Type definitions for Window.electron are located in @types/global.d.ts
 contextBridge.exposeInMainWorld("electron", {
   sendProgress: (percent) => ipcRenderer.send("progress", percent),
 } as Window["electron"]);
